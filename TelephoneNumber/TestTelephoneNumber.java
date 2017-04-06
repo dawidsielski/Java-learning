e06bfd746b1c0b39521c54a458839b54ae5322ba
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-06 13:17:57 +0200

    Map fix

--- TelephoneNumber/TestTelephoneNumber.java
+++ TelephoneNumber/TestTelephoneNumber.java
@@ -6,27 +6,19 @@ public class TestTelephoneNumber{
         Person p1 = new Person();
         Person p2 = new Person("Dawid", "Sielski","lsdkfjsldjkf", new TelephoneNumber(34,234567));
 
-        Company c1 = new Company("Dupek", "address", new TelephoneNumber(90,2567));
+        Company c1 = new Company("Polfarma", "Wilesna 1/1", new TelephoneNumber(90,2567));
 
         treeMap.put(p1.getTelephoneNumber(), p1);
         treeMap.put(p2.getTelephoneNumber(), p2);
         treeMap.put(c1.getTelephoneNumber(), c1);
 
-        // for(Map.Entry<TelephoneNumber,TelephoneEntry> entry : treeMap.entrySet()) {
-        //     TelephoneNumber key = entry.getKey();
-        //     TelephoneEntry value = entry.getValue();
-
-        //     System.out.println(key.getFullNumber() + " => " + value.description());
-        // }
-
-
-        Set set = treeMap.entrySet();
-        Iterator it = set.iterator();
+        Set<Map.Entry<TelephoneNumber,TelephoneEntry>> set = treeMap.entrySet();
+        Iterator<Map.Entry<TelephoneNumber,TelephoneEntry>> it = set.iterator();
 
         while(it.hasNext()) {
-        Map.Entry me = (Map.Entry)it.next();
-            System.out.print("Key is: "+ ((TelephoneNumber)me.getKey()).getFullNumber() + " & ");
-            System.out.println("Value is: "+ ((TelephoneEntry)me.getValue()).description());
+        Map.Entry<TelephoneNumber,TelephoneEntry> me = it.next();
+            System.out.print("Key is: "+ me.getKey().getFullNumber() + " & ");
+            System.out.println("Value is: "+ me.getValue().description());
         } 
     }
 }
